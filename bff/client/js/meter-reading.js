6061ed77943db1fc81918a86ccc9e4d14197b5cf
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2018-08-27 08:19:45 +1000

    wired up form frontend

--- bff/client/js/meter-reading.js
+++ bff/client/js/meter-reading.js
@@ -20,6 +20,19 @@ document.addEventListener('DOMContentLoaded', function() {
         Mustache.parse(template)
         var rendered = Mustache.render(template, { rows:  tableRows })
         $('#table-container').html(rendered)
+
+        $('input.save').click((event) => {
+          const deviceChannelId = event.target.dataset['deviceChannelId']
+
+          const displayNameInput = $(`tr[data-device-channel-id="${deviceChannelId}"] input.display-name`)
+          const displayNameNewValue = displayNameInput[0].value
+          const displayNameOriginalValue = displayNameInput[0].dataset['originalValue']
+
+          const litresPerPulseInput = $(`tr[data-device-channel-id="${deviceChannelId}"] input.litres-per-pulse`)
+          const litresPerPulseNewValue = litresPerPulseInput[0].value
+          const litresPerPulseOriginalValue = litresPerPulseInput[0].dataset['originalValue']
+          
+        })
       })
     })
 
